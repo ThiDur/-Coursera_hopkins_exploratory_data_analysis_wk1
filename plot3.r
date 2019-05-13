797a5ee50ef1f77eea9fd6518b51414d83ccc019
NL\TDurieux <tdurieux@deloitte.nl>
2019-05-13 19:45:57 +0200

    finally found a way to make plot 3 legend look good

--- plot3.r
+++ plot3.r
@@ -4,9 +4,9 @@ tbl_power_consumption <- read_csv2(unz("exdata_data_household_power_consumption.
 tbl_power_consumption <- filter(tbl_power_consumption, Date=='2007-02-01' | Date=='2007-02-02')
 tbl_power_consumption <- mutate(tbl_power_consumption, datetime=as.POSIXct(paste(Date, " ", Time)))
 
+png("plot3.png")
 with(tbl_power_consumption, plot(datetime, Sub_metering_1/1000, type='l', xlab='', ylab='Energy sub metering'))
 with(tbl_power_consumption, lines(datetime, Sub_metering_2/1000, col='red'))
 with(tbl_power_consumption, lines(datetime, Sub_metering_3/1000, col='blue'))
 legend('topright', legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), col=c('black', 'red', 'blue'), lty=c(1,1))
-dev.copy(png, file='plot3.png', width=480, height=480)
 dev.off()
